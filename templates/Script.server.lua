afe33b7e5d6783228e70638179444230f218c703
Vertic <thisistherealequinoctic@gmail.com>
2022-06-28 17:52:22 +0800

    Update .json files to .jsonc and add Templates

---
+++ templates/Script.server.lua
@@ -0,0 +1,23 @@
+-- Services
+
+local service = game:GetService("Service")
+
+-- Objects
+
+local obj = value
+
+
+-- Functions
+
+local function foo()
+	-- Code Block
+end
+
+
+-- Service Functions
+
+service.DoSomething:Connect(foo)
+-- Or
+service.DoSomething:Connect(function()
+	-- Code Block
+end)
